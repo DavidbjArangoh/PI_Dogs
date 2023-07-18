bf6cb6d41986703dbd68a62cc910ff4fdfe61b54
DavidbjArangoh <94635996+DavidbjArangoh@users.noreply.github.com>
2023-07-18 02:39:29 -0500

    Avance en los detalles de los handlers y rutas

--- api/src/handlers/dogHandlers.js
+++ api/src/handlers/dogHandlers.js
@@ -4,22 +4,54 @@ const axios = require("axios");
 const {APIKEY} = process.env;
 const apiBreeds = `https://api.thedogapi.com/v1/breeds?${APIKEY}`
 
+// Revisar y programar apropiadamente los handlers, 
+// las conexiones estan bien ejecutadas y pueden traerse apropiadamente los datos de la API
 
 const getDogsHandler = async (req,res) => {
-
-    try {
-        
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
+    axios.get(apiBreeds)
+        .then(resp => {
+            const data = resp.data;
+            console.log(data);
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({error:err.message})
+        }) 
+    
 }
 
 const getRaceHandler = async (req,res) => {
-
+    try {
+        const raceid = req.params;
+        console.log(req.params);
+        axios.get(apiBreeds)
+        .then(resp => {
+            const data = resp.data;
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({error:err.message})
+        })
+    } catch (error) {
+        res.status(400).json({message:error})
+    }
 }
 
 const getNameHandler = async (req,res) => {
-
+    try {
+        const raza_perro = req.params;
+        axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${raza_perro}${APIKEY}`)
+        .then(resp => {
+            const data = resp.data;
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({error:err.message})
+        })
+        res.status(200).json({message:"se hizo la request"})
+    } catch (error) {
+        res.status(400).json({message:error})
+    }
 }
 
 const postDogsHandler = async (req,res) => {
@@ -30,8 +62,7 @@ const postDogsHandler = async (req,res) => {
         );
         res.status(200).json(response);
     } catch (error) {
-        console.log("something");
-        res.status(400).json({message:"wrong"});
+        res.status(400).json({message:error});
     }
 }
 
