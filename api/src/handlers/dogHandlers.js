523ccc610c7cd72e50e5e0428371faa15ad84e1b
DavidbjArangoh <94635996+DavidbjArangoh@users.noreply.github.com>
2023-07-19 04:25:09 -0500

    TempHandler y finalizando el getDogs hanadler

--- api/src/handlers/dogHandlers.js
+++ api/src/handlers/dogHandlers.js
@@ -1,6 +1,5 @@
 require("dotenv").config;
-const { query } = require("express");
-const {Dog} = require("../db");
+const {Dog, Temperament} = require("../db");
 const axios = require("axios");
 const {APIKEY} = process.env;
 const apiBreeds = `https://api.thedogapi.com/v1/breeds?=${APIKEY}`
@@ -10,15 +9,53 @@ const URL = "https://api.thedogapi.com/v1/breeds/search?q="
 // las conexiones estan bien ejecutadas y pueden traerse apropiadamente los datos de la API
 
 const getDogsHandler = async (req,res) => {
-    axios.get(apiBreeds)
-    .then(resp => {
-        const data = resp.data;
-        res.status(200).json(data)
-    })
-    .catch(err => {
-        res.status(400).json({error:err.message})
-    }) 
-    
+    const getDBDogs = async () => {
+        try{
+            var dogsDB = await Dog.findAll({
+                include: {
+                    model: Temperament,
+                    attributes: ['name'],
+                    through: {
+                        attributes: [],
+                    }
+                }
+            })
+            return dogsDB;
+        }
+        catch (err) {
+            console.log("Error fetching DB dogs:", err);
+            throw err;
+        }
+    }
+    const getApiDogs = async () => {
+        try{
+            const resp = await axios.get(apiBreeds)
+            return resp.data.map(dog => ({
+                        id: dog.id,
+                        name: dog.name,
+                        image: dog.image.url,
+                        breed_group: dog.breed_group,
+                        temperament: dog.temperament,
+                        life_span: dog.life_span,
+                        weight_min: parseInt(dog.weight.metric.slice(0, 2).trim()),
+                        weight_max: parseInt(dog.weight.metric.slice(4).trim()),
+                        height_min: parseInt(dog.height.metric.slice(0, 2).trim()),
+                        height_max: parseInt(dog.height.metric.slice(4).trim()),
+                    }
+                )
+            )
+        } catch (err) {
+            console.log("Error fetching API dogs", err);
+        }
+    };
+    try {
+        const apiDogs = await getApiDogs();
+        const DBDogs = await getDBDogs();
+        const totalDogs = apiDogs.concat(DBDogs);
+        res.status(200).send(totalDogs);
+    } catch (error) {
+        res.status(400).json({"error":error});
+    };
 }
 
 const getRaceHandler = async (req,res) => {
--- api/src/handlers/tempHandlers.js
+++ api/src/handlers/tempHandlers.js
@@ -1,12 +1,26 @@
 require("dotenv").config;
-const {Temperaments} = require("../db");
+const {Temperament} = require("../db");
 const axios = require("axios");
 const {APIKEY} = process.env;
-const apiBreeds = `https://api.thedogapi.com/v1/breeds?${APIKEY}`
+const apiBreeds = `https://api.thedogapi.com/v1/breeds?api_key=${APIKEY}`
 
 
 const getTempHandler = async (req,res) =>{
-    
-}
+    await axios.get(apiBreeds)
+    .then(async resp => {
+        let everyTemperament = resp.data.map(dog => dog.temperament ? dog.temperament : "").map(temp => temp.split(', '))
+        let eachTemperament = [...new Set(everyTemperament.flat())];
+        eachTemperament.forEach(el => {
+            Temperament.findOrCreate({
+                where: {name: el}
+            })
+        });
+        eachTemperament = await Temperament.findAll()
+        res.status(200).json(eachTemperament);
+    })
+    .catch (error => {
+        res.status(404).send(error)
+    });
+};
 
 module.exports = getTempHandler;
