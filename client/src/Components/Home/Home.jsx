2dbd155fcabeace55ae8151fa1801a5683c9ff21
DavidbjArangoh <94635996+DavidbjArangoh@users.noreply.github.com>
2023-07-19 01:47:42 -0500

    Creación de componentes, reducers, actions y store

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styles from "./Home.module.css";
+import NavBar from "../NavBar/NavBar";
 
-export default function LandingPage(){
+
+export default function Home(){
     return (
-        <div id = {styles.Principal}>
-            <NavBar/>
-        </div>
-    )
+        <Fragment>
+            <header>
+                Este será el header
+            </header>
+            <div id = {styles.Principal}>
+                <NavBar/>
+            </div>
+
+        </Fragment>
+        
+    );
 }
